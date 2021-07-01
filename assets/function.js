275af7a54e630bb943dbcb273a43e225e854c712
Camden McLeod <74691129+ccmcleod05@users.noreply.github.com>
2021-07-01 18:50:51 -0500

    Delete assets directory

--- assets/function.js
+++
@@ -1,5 +0,0 @@
-const githubStats = document.querySelector("#github-stats");
-
-githubStats.addEventListener("onclick", () => {
-    window.open("https://github.com/anuraghazra/github-readme-stats")
-});
